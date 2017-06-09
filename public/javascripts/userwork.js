be88dd441e71f2e576968bb608b1eb2f070bd79f
Xdoge <435199523@qq.com>
2017-06-09 22:24:49 +0800

    xjl
    主页页面

--- public/javascripts/userwork.js
+++ public/javascripts/userwork.js
@@ -90,3 +90,69 @@ function login_denglu (argument) {
   }
   createCode();
 }
+
+function reregister () {
+  for(var i = 0; i < localStorage.length; i++) {
+        if (phone == localStorage.key(i)) {
+          alert("该用户已注册");
+          return false;
+        }
+    }
+    return true;
+}
+
+function register () {
+  phone = document.getElementById("phone_input").value;
+  password = document.getElementById("password_input").value;
+  if (check()) {
+    if (validateCode()) {
+      if (validate_phone(phone) == true) {
+        if (reregister()) {
+          localStorage.setItem(phone, password);
+          alert("注册成功");
+          document.getElementById("phone_input").value = "";
+          document.getElementById("password_input").value = "";
+        }
+      } else {
+        alert("请输入正确的手机号码");
+      }
+    }
+  }
+  createCode();
+}
+
+var code;
+function createCode() {
+  code = "";
+  var codeLength = 6; //验证码的长度
+  var checkCode = document.getElementById("checkCode");
+  var codeChars = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //所有候选组成验证码的字符，当然也可以用中文的
+  for(var i = 0; i < codeLength; i++) {
+    var charNum = Math.floor(Math.random() * 52);
+    code += codeChars[charNum];
+  }
+  if(checkCode) {
+      checkCode.className = "code";
+      checkCode.innerHTML = code;
+  }
+  document.getElementById("code_input").value = "";
+}
+
+function validateCode() {
+  var code_input = document.getElementById("code_input").value;
+  if(code_input.length <= 0) {
+      alert("请输入验证码！");
+      return false;
+  } else if (code_input.toUpperCase() != code.toUpperCase()) {
+      alert("验证码输入有误！");
+      createCode();
+      return false;
+  } else {
+      return true;
+  }    
+}
+
+function user_clear () {
+  localStorage.clear();
+}
